99ccd770a55df84a3a6ab42f953aafa572809ce5
miasergi <miasergi@users.noreply.github.com>
2026-04-04 01:58:20 +0200

    deploy: move static site to repo root - serve directly from main branch

---
+++ _next/static/chunks/app/layout-30f9aab2726d57e2.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7177],{3673:()=>{},4097:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_f367f3"}},7391:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,4097,23)),Promise.resolve().then(l.t.bind(l,3673,23))}},e=>{e.O(0,[7898,8441,1255,7358],()=>e(e.s=7391)),_N_E=e.O()}]);
